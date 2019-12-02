1c07829e84eaee5685a11fd9ff22c7f76bbb0986
Vanický Adam <vanicky.adam.2017@skola.ssps.cz>
2019-12-02 16:19:00 +0100

    Přidejte soubory projektu.

---
+++ Autonomní auta/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autonomní_auta
+{
+    class Program
+    {
+        public Trasa zmena = Trasa.Cesta;
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    
+
+    public class AutonomniAuto
+    {
+        
+
+        public int CestovniRychlost { get; set; }
+        public int DelkaTrasy { get; set; }
+
+        public AutonomniAuto(int cestrovnirychlost, int delkatrasy)
+        {
+            CestovniRychlost = cestrovnirychlost;
+            DelkaTrasy = delkatrasy;
+        }
+    }
+
+    public class MeteorologickaStanice
+    {
+
+    }
+
+    public class RidiciStredisko
+    {
+
+    }
+
+    public enum Trasa
+    {
+        Cesta = 1,
+        Most,
+        Tunel
+    }
+}
